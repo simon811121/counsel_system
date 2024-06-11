b8286e401f8da9a1f7ec4bc98d629dc879b1b874
Simon Huang <simon811121@gmail.com>
2024-06-11 23:46:08 +0800

    Refine data.py & add find_acnt_info by user_id

--- src/data.py
+++ src/data.py
@@ -6,14 +6,16 @@ from enum import Enum
 from runtime_type_checker import check_type
 from typing import Any, Dict
 
-config_file_name = "config.json"
-acnt_info_file_name = "account_info.csv"
 acnt_info_fieldnames = ['user_id', 'permission', 'permission_id', 'account', 'password', 'name', 'email', 'phone_num', 'register_time', 'last_login_time']
-allowed_permissions = {'director', 'psychologist', 'manager', 'guest'}
+class Allow_Perm(Enum):
+    director = 1
+    psychologist = 2
+    manager = 3
+    guest = 4
 
 class Config:
     def __init__(self):
-        file_path = gen_file_path(config_file_name)
+        file_path = gen_file_path("config.json")
         self.file_path = file_path
         self.settings = self._read_settings()
 
@@ -39,12 +41,6 @@ class Config:
             json.dump(self.settings, file, indent=4)
 
 
-class Allow_Perm(Enum):
-    director = 1
-    psychologist = 2
-    manager = 3
-    guest = 4
-
 class Account_Info:
     class Info:
         def __init__(self,
@@ -66,7 +62,8 @@ class Account_Info:
             check_type(phone_num, str)
             check_type(register_time, str)
             check_type(last_login_time, str)
-            if permission not in allowed_permissions:
+            self.allow_perm = {'director', 'psychologist', 'manager', 'guest'}
+            if permission not in self.allow_perm:
                 raise ValueError("Invalid permission value. Allowed values: 'director', 'psychologist', 'manager', 'guest'")
             permission_id = Allow_Perm[permission]
             self.user_id = user_id
@@ -82,17 +79,22 @@ class Account_Info:
 
     def __init__(self):
         self.file_name = "account_info.csv"
+        self.file_path = gen_file_path(self.file_name)
         self._build_file()
         self.infos = []
         self.glb_cfg = Config()
         self.setting = self.glb_cfg.get_section('acnt_info')
 
     def _build_file(self):
-        file_path = gen_file_path(self.file_name)
-        if not os.path.exists(file_path):
-            with open(file_path, 'w', newline='') as file:
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w', newline='') as file:
                 writer = csv.DictWriter(file, fieldnames=acnt_info_fieldnames)
                 writer.writeheader()
+
+    def _refresh_file(self): # only for unit-testing
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+            self._build_file()
 
     def add_acnt_info(self,
                       permission: str,
@@ -111,10 +113,20 @@ class Account_Info:
             user_id += 1
             self.glb_cfg.update_setting('acnt_info', 'nxt_user_id', user_id)
             self.glb_cfg.save_settings()
-            file_path = gen_file_path(self.file_name)
-            with open(file_path, 'a', newline='') as file:
+            self.file_path = gen_file_path(self.file_name)
+            with open(self.file_path, 'a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow([info.user_id, info.permission, info.permission_id, info.account, info.password, info.name, info.email, info.phone_num, info.register_time, info.last_login_time])
+
+    def find_acnt_info(self,
+                       user_id: int):
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                # Check if the user_id matches
+                if int(row['user_id']) == user_id:
+                    return row
+            return None
 
 def gen_file_path(file_name):
     cur_path = os.getcwd()
@@ -123,6 +135,9 @@ def gen_file_path(file_name):
 
 if __name__ == '__main__':
     acnt_info = Account_Info()
+    acnt_info.glb_cfg.update_setting('acnt_info', 'nxt_user_id', 0)
+    acnt_info.glb_cfg.save_settings()
+    acnt_info._refresh_file()
     cur_time = datetime.datetime.now()
     for_time = cur_time.strftime("%Y-%m-%d %H:%M:%S")
     acnt_info.add_acnt_info('director', 'abcd', 'qazwsx', 'Simon', 's@example.com', '12345678', for_time, for_time)
@@ -132,3 +147,6 @@ if __name__ == '__main__':
     cur_time = datetime.datetime.now()
     for_time = cur_time.strftime("%Y-%m-%d %H:%M:%S")
     acnt_info.add_acnt_info('manager', 'efgh', 'edcrfv', 'Jing', 'J@example.com', '87654321', for_time, for_time)
+    print(acnt_info.find_acnt_info(0))
+    print(acnt_info.find_acnt_info(1))
+    print(acnt_info.find_acnt_info(2))
